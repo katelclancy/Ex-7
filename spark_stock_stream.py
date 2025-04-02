606ca5a80f58ae9a13b001afe12839dba59b3bbf
katelclancy <katelclancy@gmail.com>
2025-04-02 00:24:10 -0400

    updated to fix parsing

--- spark_stock_stream.py
+++ spark_stock_stream.py
@@ -3,63 +3,48 @@
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import split, col
-from pyspark.sql.types import StructType, StringType, DoubleType, TimestampType
+from pyspark.sql.types import TimestampType, DoubleType
 
-# Create Spark session with streaming support
+# Initialize Spark session
 spark = SparkSession.builder \
     .appName("StockPriceStreaming") \
     .getOrCreate()
 
-# Set log level lower to avoid clutter
 spark.sparkContext.setLogLevel("WARN")
 
-# Read data from the socket
+# Read from socket
 lines = spark.readStream \
     .format("socket") \
     .option("host", "localhost") \
     .option("port", 9999) \
     .load()
-    
-# Show raw values
-query = lines.writeStream \
+
+# Split incoming lines like: "2021-01-04 09:30:00, AAPL, 129.41"
+split_cols = split(lines["value"], ",")
+
+# Parse and cast columns
+df = lines.select(
+    split_cols.getItem(0).alias("datetime"),
+    split_cols.getItem(1).alias("symbol"),
+    split_cols.getItem(2).cast(DoubleType()).alias("close")
+).withColumn("datetime", col("datetime").cast(TimestampType()))
+
+# Filter into two separate streams
+aapl_df = df.filter(col("symbol") == "AAPL")
+msft_df = df.filter(col("symbol") == "MSFT")
+
+# Show both AAPL and MSFT streams to console (just for testing)
+aapl_query = aapl_df.writeStream \
     .outputMode("append") \
     .format("console") \
+    .option("truncate", False) \
     .start()
-    
-# Split CSV string: "2021-01-04 09:30:00, AAPL, 129.41"
-# schema = StructType() \
-#     .add("datetime", StringType()) \
-#     .add("symbol", StringType()) \
-#     .add("close", StringType())
 
-# split_cols = split(lines.value, ",")
+msft_query = msft_df.writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .option("truncate", False) \
+    .start()
 
-# Apply schema
-# df = lines.select(
-#     split_cols.getItem(0).alias("datetime"),
-#     split_cols.getItem(1).alias("symbol"),
-#     split_cols.getItem(2).cast("double").alias("close")
-# )
-
-# # Cast datetime to TimestampType
-# df = df.withColumn("datetime", col("datetime").cast(TimestampType()))
-
-# # Filter for AAPL and MSFT
-# aapl_df = df.filter(col("symbol") == "AAPL")
-# msft_df = df.filter(col("symbol") == "MSFT")
-
-# # Output to console for testing
-# aapl_query = aapl_df.writeStream \
-#     .outputMode("append") \
-#     .format("console") \
-#     .option("truncate", False) \
-#     .start()
-
-# msft_query = msft_df.writeStream \
-#     .outputMode("append") \
-#     .format("console") \
-#     .option("truncate", False) \
-#     .start()
-
+# Wait for termination
 spark.streams.awaitAnyTermination()
-
